5acdb247eebe9049ccd87e4cad72f7253907bd3c
Àngel <angel@rom.cat>
2022-09-06 20:06:58 +0200

    Agregar archivos de proyecto.

---
+++ Datos/Class1.cs
@@ -0,0 +1,102 @@
+﻿using Datos.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Class1
+    {
+        private static NorthWindTuneadoDbContext _db = null;
+
+        public Class1()
+        {
+            if (_db == null)
+            {
+                _db = new NorthWindTuneadoDbContext();
+            }
+        }
+
+        public IList<Categoria> DevuelveCategorias()
+        {
+            IList<Categoria> categorias = null;
+            categorias = _db.Categoria.ToList();
+
+            return categorias;
+        }
+
+        public bool GuardarRegistro(Categoria categoria)
+        {
+            bool ok = false;
+            try
+            {
+                _db.Categoria.Add(categoria);
+                int respuesta = 0;
+                respuesta = _db.SaveChanges();
+                if (respuesta > 0)
+                {
+                    ok = true;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return ok;
+        }
+
+        public bool GuardarRegistro(int id, string texto)
+        {
+            bool ok = false;
+            Categoria categoria = null;
+            //categoria = _db.Categoria.Find(id);
+            categoria = _db.Categoria.Where(x => x.CategoryID == id).FirstOrDefault();
+            categoria.CategoryName = texto;
+            try
+            {
+                int respuesta = 0;
+                respuesta = _db.SaveChanges();
+                if (respuesta > 0)
+                {
+                    ok = true;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return ok;
+        }
+
+        public bool GuardarRegistro(int id)
+        {
+            bool ok = false;
+            Categoria categoria = null;
+            categoria = _db.Categoria.Where(x => x.CategoryID == id).FirstOrDefault();
+            _db.Categoria.Remove(categoria);
+            try
+            {
+                int respuesta = 0;
+                respuesta = _db.SaveChanges();
+                if (respuesta > 0)
+                {
+                    ok = true;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return ok;
+        }
+
+    }
+}
---
+++ Principal/Program.cs
@@ -0,0 +1,120 @@
+﻿using Datos;
+using Datos.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.AnyadeCategoria();
+            program.Comprobar();
+            program.Modificar();
+            program.Comprobar();
+            program.Eliminar();
+            program.Comprobar();
+        }
+
+        //INSERT O CREATE
+        private void AnyadeCategoria()
+        {
+            Class1 class1 = new Class1();
+            //IList<Categoria> categorias = class1.DevuelveCategorias();
+
+            Categoria categoria = new Categoria();
+            //categoria.CategoryID = 1;
+            categoria.CategoryName = "Chisme";
+            categoria.Description = "Producto de escaso valor";
+
+            //categorias.Add(categoria);
+
+            bool ok = false;
+            ok = class1.GuardarRegistro(categoria);
+
+            Console.WriteLine("El resultado de la inserción ha sido: " + ok);
+
+            Console.ReadLine();
+        }
+
+        //SELECT O READ
+        private void Comprobar()
+        {
+            Class1 class1 = new Class1();
+            IList<Categoria> categorias = class1.DevuelveCategorias();
+            foreach (Categoria categoria in categorias)
+            {
+                Console.WriteLine(categoria.CategoryID + " " + categoria.CategoryName);
+            }
+
+            Console.ReadLine();
+        }
+
+        //UPDATE
+        private void Modificar()
+        {
+            Class1 class1 = new Class1();
+            IList<Categoria> categorias = class1.DevuelveCategorias();
+
+
+            foreach (Categoria categoria in categorias)
+            {
+                Console.WriteLine(categoria.CategoryID + " " + categoria.CategoryName);
+            }
+
+            Console.WriteLine("Dime el id del registro a modificar");
+            string texto = Console.ReadLine();
+            int id = 0;
+            if (int.TryParse(texto, out id) == true)
+            {
+                id = int.Parse(texto);
+            }
+            Console.WriteLine("Ahora, Dime el nuevo nombre");
+
+            texto = Console.ReadLine();
+
+            bool ok = false;
+            ok = class1.GuardarRegistro(id, texto);
+
+            Console.WriteLine("El resultado de la modificación ha sido: " + ok);
+
+            Console.ReadLine();
+        }
+
+        //DELETE
+        private void Eliminar()
+        {
+            Class1 class1 = new Class1();
+            IList<Categoria> categorias = class1.DevuelveCategorias();
+
+
+            foreach (Categoria categoria in categorias)
+            {
+                Console.WriteLine(categoria.CategoryID + " " + categoria.CategoryName);
+            }
+
+            Console.WriteLine("Dime el id del registro a eliminar");
+            string texto = Console.ReadLine();
+            int id = 0;
+            if (int.TryParse(texto, out id) == true)
+            {
+                id = int.Parse(texto);
+            }
+            //Console.WriteLine("Ahora, Dime el nuevo nombre");
+
+            //texto = Console.ReadLine();
+
+            bool ok = false;
+            ok = class1.GuardarRegistro(id);
+
+            Console.WriteLine("El resultado de la eliminación ha sido: " + ok);
+
+            Console.ReadLine();
+        }
+    }
+}
